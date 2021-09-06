29ac852de187d76e0fe06119463f868bf1667fa0
Winston Kinoti <82153241+WinTush@users.noreply.github.com>
2021-09-06 18:40:08 +0300

    Regular expressions

--- 03 - regex/regex.py
+++ 03 - regex/regex.py
@@ -1,8 +1,22 @@
 import re
 
-
-text = 'This is a string containing letters and numbers. 0123456789.'
 log = 'July 31 07:51:48 mycomputer bad_process[12345]: ERROR: Performing package upgrade'
 regex = r'\[(\d+)\]'
 result = re.search(regex, log)
 print(result[1])
+print()
+
+
+def check_aei(text):
+    return re.search(r"a.e.i", text) is not None
+
+
+print(check_aei("academia"))  # True
+print(check_aei("aerial"))  # False
+print(check_aei("paramedic"))  # True
+print()
+
+print(re.search(r'p.ng', 'penguin'))
+print(re.search(r'p.ng', 'clapping'))
+print(re.search(r'p.ng', 'sponge'))
+print(re.search(r'p.ng', 'Pangaea', re.IGNORECASE))
